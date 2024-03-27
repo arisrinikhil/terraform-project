51091687825500146aa7c5fc473f74c3bd03062b
arisrinikhil <152692307+arisrinikhil@users.noreply.github.com>
2024-03-27 18:00:38 +0530

    Update elb.tf

--- elb.tf
+++ elb.tf
@@ -25,6 +25,27 @@ resource "aws_elb" "bar" {
   }
 }
 
+resource "aws_subnet" "database-subnet-1" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.21.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Database-1a"
+  }
+}
+
+resource "aws_subnet" "database-subnet-2" {
+  vpc_id            = aws_vpc.my-vpc.id
+  cidr_block        = "10.0.22.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Database-2b"
+  }
+}
+
+
  resource "aws_rds_cluster" "default" {
   cluster_identifier      = "aurora-cluster-demo"
   engine                  = "aurora-mysql"
